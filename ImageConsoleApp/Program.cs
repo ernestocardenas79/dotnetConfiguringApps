b78c3db8d16b7eed38aa3eb66d7a400dfd12d637
Ernesto Cardenas Llanes <ernesto.cardenas79@gmail.com>
2024-12-18 21:20:43 -0700

    configuration builder added

--- ImageConsoleApp/Program.cs
+++ ImageConsoleApp/Program.cs
@@ -1,3 +1,10 @@
 ﻿using System;
 
 Console.WriteLine("Hello World!");
+IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+configurationBuilder.AddJsonFile("config.json");
+
+IConfigurationRoot configuration = configurationBuilder.Build();
+
+Console.WriteLine("***** Process Image *****");
+Console.WriteLine($"Processing {args[0]}");
