da4e98c4fb21efcca066d54b69e7b015878d88a5
Ernesto Cardenas Llanes <ernesto.cardenas79@gmail.com>
2024-12-18 21:21:59 -0700

    compresion configurations added, config file created

--- ImageConsoleApp/Program.cs
+++ ImageConsoleApp/Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 
-Console.WriteLine("Hello World!");
+
 IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
 configurationBuilder.AddJsonFile("config.json");
 
@@ -8,3 +9,16 @@ IConfigurationRoot configuration = configurationBuilder.Build();
 
 Console.WriteLine("***** Process Image *****");
 Console.WriteLine($"Processing {args[0]}");
+
+Console.WriteLine($"Thumbnail Width: {configuration["THUMBNAILWIDTH"]}");
+Console.WriteLine($"Thumbnail FilePrefix: {configuration["thumbnailFilePrefix"]}");
+
+Console.WriteLine($"Medium Width: {configuration["mediumWidth"]}");
+Console.WriteLine($"Medium FilePrefix: {configuration["mediumFilePrefix"]}");
+
+Console.WriteLine($"Large Width: {configuration["largeWidth"]}");
+Console.WriteLine($"Large FilePrefix: {configuration["largeFilePrefix"]}");
+
+Console.WriteLine($"Watermark: {configuration["watermarkText"]}");
+
+Console.WriteLine($"Compression Level: {configuration["compressionLevel"]}");
